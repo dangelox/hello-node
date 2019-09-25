bf5d9c3a340c12eb97e9e964898eabb370b1a9e5
Dilesh Fernando <dilesh.fernando@leggett.com>
2019-09-25 11:00:37 -0500

    add app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+// health check
+app.get('/status', (req, res)=>res.sendStatus(200))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
